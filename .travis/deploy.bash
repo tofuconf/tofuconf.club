f0f7094187568dd643504d519a7b8f58809c1dd2
Tiryoh <tiryoh@gmail.com>
2018-01-10 20:29:24 +0900

    Enable deploying

--- .travis/deploy.bash
+++ .travis/deploy.bash
@@ -2,4 +2,4 @@
 
 TARGET_DIR=$(cd "$(dirname "$0")"; cd ../; pwd)
 
-rsync -apz -v --dry-run "${TARGET_DIR}/docs" "travis@tofuconf.club:/var/www/html"
+rsync -apzv --exclude="CNAME" "${TARGET_DIR}/docs" "travis@tofuconf.club:/var/www/html"
