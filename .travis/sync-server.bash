59555c2e37e5ab1a6d40121e29cfe3be5e03844a
Tiryoh <tiryoh@gmail.com>
2018-01-17 22:55:31 +0900

    Fix sync error

--- .travis/sync-server.bash
+++ .travis/sync-server.bash
@@ -4,4 +4,4 @@ set -eu
 
 TARGET_DIR=$(cd "$(dirname "$0")"; cd ../_site; pwd)
 
-rsync -apzv --exclude="CNAME" "${TARGET_DIR}" "travis@tofuconf.club:/var/www/html"
+rsync -apzv --exclude="CNAME" "${TARGET_DIR}/" "travis@tofuconf.club:/var/www/html"
