9cda39ab3040d3165c97fed6cbd935653509b51b
Tiryoh <tiryoh@gmail.com>
2018-02-24 02:35:17 +0900

    Fix html-proofer error (correction of 78b3329)

--- .travis/build-html.bash
+++ .travis/build-html.bash
@@ -3,4 +3,4 @@
 set -eu
 
 JEKYLL_ENV=production bundle exec jekyll build
-# bundle exec htmlproofer --url-ignore "/#.*/" ./_site
+bundle exec htmlproofer --url-ignore "/#.*/,/tofuconf.club/.*/" ./_site
