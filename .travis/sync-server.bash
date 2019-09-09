56b53c01ffc5d72f2348e67f41589ff9d9ec7789
Tiryoh <tiryoh@gmail.com>
2019-09-10 00:08:14 +0900

    Fix sync error

--- .travis/sync-server.bash
+++ .travis/sync-server.bash
@@ -4,4 +4,4 @@ set -eu
 
 TARGET_DIR=$(cd "$(dirname "$0")"; cd ../_site; pwd)
 
-rsync -apzv --dry-run --exclude="CNAME" --exclude="renovate.json" --exclude="minimal-mistakes-jekyll.gemspec" --exclude-from=<(git ls-files --others --exclude-standard) "${TARGET_DIR}/" "travis@tofuconf.club:/var/www/html"
+rsync -apzv --exclude="CNAME" --exclude="renovate.json" --exclude="*.gemspec" --exclude-from=<(git ls-files --others --exclude-standard) "${TARGET_DIR}/" "travis@tofuconf.club:/var/www/html"
