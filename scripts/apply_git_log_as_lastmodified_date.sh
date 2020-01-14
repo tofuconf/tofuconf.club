dd3c6fda492c965babd99a8014558062c26fd5a5
Tiryoh <tiryoh@gmail.com>
2020-01-15 03:06:47 +0900

    Fix typo

--- scripts/apply_git_log_as_lastmodified_date.sh
+++ scripts/apply_git_log_as_lastmodified_date.sh
@@ -9,7 +9,7 @@ do
 	LASTMODIFY=$(git log -1 --format=%ci $FILE | sed -E "s/([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}) (\+[0-9]{2})([0-9]{2})/\1T\2\3:\4/")
 	echo "$FILE ($FILECREATED -> $LASTMODIFY)"
 	# $SRC_DIR/scripts/update_modified_date.sh $FILE
-	sed -E 's/date: [0-9]{4}-.*\+[0-9]{2}:[0-9]{2}/date: '$LASTMODIFY'/g' $FILE > /tmp/temp.md
+	sed -E 's/date: [0-9]{4}-.*\+[0-9]{2}:[0-9]{2}/date: '$FILECREATED'/g' $FILE > /tmp/temp.md
 	cp /tmp/temp.md $FILE
 	sed -E 's/last_modified_at: [0-9]{4}-.*\+[0-9]{2}:[0-9]{2}/last_modified_at: '$LASTMODIFY'/g' $FILE > /tmp/temp.md
 	cp /tmp/temp.md $FILE
