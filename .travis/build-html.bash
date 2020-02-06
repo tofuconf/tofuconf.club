bf845beba728e516248ae4b6cc3eb56bf9204dd1
Daisuke Sato <tiryoh@gmail.com>
2020-02-06 16:20:14 +0900

    Migrate TravisCI to GHA (#136)
    * chore: Add GHA deploy settings
    
    * chore: Remove TravisCI settings

--- .travis/build-html.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-./scripts/apply_git_log_as_lastmodified_date.sh
-JEKYLL_ENV=production bundle exec jekyll build
-bundle exec htmlproofer --url-ignore "/#.*/,/tofuconf.club/.*/" ./_site
--- .travis/prepare-ssh.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-mkdir -p ~/.ssh && touch ~/.ssh/config
-echo -e "Host tofuconf.club\n\tStrictHostKeyChecking no\n\tIdentityFile ~/.ssh/id_rsa\n\tPort 22001\n" >> ~/.ssh/config
-openssl aes-256-cbc -K ${encrypted_e8f8f0f19301_key} -iv ${encrypted_e8f8f0f19301_iv} -in ./.travis/id_rsa.enc -out ./.travis/id_rsa -d
-chmod 600 ./.travis/id_rsa
-cp ./.travis/id_rsa ~/.ssh/
