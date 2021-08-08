41dae1bfd99ad490a6607c85d6113d65199d8c4f
sinuture <mokanonn2525@gmail.com>
2021-08-08 10:07:34 +0900

    URL訂正、公開日、最終編集日、運営情報追加

--- scripts/update_modified_date.sh
+++ scripts/update_modified_date.sh
@@ -6,3 +6,6 @@ LASTMODIFY=$(date '+%Y-%m-%dT%H:%M:%S%z' | sed -E 's/0900$/09:00/g')
 echo "$1 ($LASTMODIFY)"
 sed -E 's/last_modified_at: [0-9]{4}-.*\+[0-9]{2}:[0-9]{2}/last_modified_at: '$LASTMODIFY'/g' $1 > /tmp/temp.md
 cp /tmp/temp.md $1
+
+# 使用例
+# $ ./update_modified_date.sh ../_posts/2021-08-07-11th-tofuconf-general.md
