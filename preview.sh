105b488e2eb1550d943a874bef7412e6c857525a
Daisuke Sato <tiryoh@gmail.com>
2021-12-29 14:58:53 +0900

    chore: Update preview script

--- preview.sh
+++ preview.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -eu
 
-if $(command -v bundle> /dev/null 2>&1) ; then
-    if $(command -v bundle exec jekyll > /dev/null 2>&1) ; then
+if $(bundle > /dev/null 2>&1) ; then
+    if $(bundle exec jekyll > /dev/null 2>&1) ; then
         bundle exec jekyll serve --config _config.yml --host='127.0.0.1'  --trace
     else
         bundle install --path vendor/bundle
         bundle exec jekyll serve --config _config.yml --host='127.0.0.1'  --trace
     fi
 else
-    JEKYLL_VERSION=3.8
+    JEKYLL_VERSION=3.8.0
     docker run --rm \
     --volume="$PWD:/srv/jekyll" \
     -p 4000:4000 \
-    -it jekyll/jekyll:$JEKYLL_VERSION \
+    -it tiryoh/jekyll:$JEKYLL_VERSION \
     jekyll serve --config _config.yml
 fi
